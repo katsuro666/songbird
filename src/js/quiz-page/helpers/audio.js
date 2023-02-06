4045bf7e21c1855b12fc7ede5305c7d7a465a24a
katsuro666 <kreepinoutdakut@gmail.com>
2023-02-06 18:45:05 +0300

    refactor: remove extra lines

--- src/js/quiz-page/helpers/audio.js
+++ src/js/quiz-page/helpers/audio.js
@@ -2,15 +2,10 @@ let correct = document.querySelector('.audio-correct')
 let wrong = document.querySelector('.audio-wrong')
 
 function correctAnswer(){
-  // if (!this.classList.contain('--correct')) {
     new Audio(correct.src).play()
-  // }
 }
 function wrongAnswer(){
-  // if (!this.classList.contain('--wrong')) {
     new Audio(wrong.src).play()
-    console.log(this)
-  // }
 }
 
 export {correctAnswer, wrongAnswer}
